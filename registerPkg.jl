63ee968135a4f916599fa2539431aa21150d7b96
zeal yen <zealyen@noodoe.com>
2024-10-02 17:37:41 +0800

    up

--- registerPkg.jl
+++ registerPkg.jl
@@ -1,6 +1,7 @@
 pkgName = ARGS[1]
+repoToken = ARGS[2]
 registryRepoUrl = "git@github.com:sojoasd/My-Julia.git"
-registryPublicRepoUrl = "https://github.com/sojoasd/My-Julia.git"
+registryPublicRepoUrl = "https://$repoToken@github.com/sojoasd/My-Julia.git"
 
 pkgDir = "$(@__DIR__)/$pkgName"
 println("$pkgDir")
@@ -21,7 +22,7 @@ if !isfile("Registry.toml")
     println("Registry.toml created")
 end
 
-LocalRegistry.register(pkgDir; registry = registryRepoUrl, repo = registryPublicRepoUrl)
+LocalRegistry.register(pkgDir; registry = registryPublicRepoUrl, repo = registryPublicRepoUrl)
 println("Package registered")
 
 
