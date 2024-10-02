992c4de1ea7cf71799c4f47d7a52524543eedde8
zeal yen <sojoasd@gmail.com>
2024-10-02 18:39:43 +0800

    up

--- registerPkg.jl
+++ registerPkg.jl
@@ -1,7 +1,7 @@
 pkgName = ARGS[1]
 repoToken = ARGS[2]
 registryRepoUrl = "git@github.com:sojoasd/My-Julia.git"
-registryPublicRepoUrl = "https://$repoToken@github.com/sojoasd/My-Julia.git"
+registryPublicRepoUrl = "https://x-access-token:$repoToken@github.com/sojoasd/My-Julia.git"
 
 pkgDir = "$(@__DIR__)/$pkgName"
 println("$pkgDir")
@@ -13,6 +13,10 @@ using Pkg
 Pkg.instantiate()
 
 using LocalRegistry
+
+# Set Git user configuration
+run(`git config --global user.email "sojoasd@gmail.com"`)
+run(`git config --global user.name "zeal yen"`)
 
 # check Registry.toml exists
 if !isfile("Registry.toml")
